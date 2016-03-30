965abce62063f8ebc1ad5fa2405c6bd11167b6f2
Rich Zitterman <richzitterman@gmail.com>
2016-03-30 01:14:46 -0400

    adding game changes

--- app.js
+++ app.js
@@ -4,6 +4,20 @@
 // Student @GA in NYC
 //  -->
 
+var cardArray = ["joker", "queen", "joker"];
+
+// Durstenfeld shuffle algorithm
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+  return array;
+};
+
+var money = 0;
 
 var cardShow = function() {
   $(".one").addClass("joker");
@@ -38,45 +52,74 @@ var playButton = function() {
   });
 };
 
-var cardArray = ["joker", "queen", "joker", "joker"];
-var randomCard = cardArray[Math.floor(Math.random() * cardArray.length)];
-
-var choiceOutcome = function() {
-  if (randomCard === cardArray[1]) {
-     $("#message-display p").text("WOW, I can't believe you found her!");
-     updateMoney();
-   } else if (randomCard !== cardArray[1]) {
-     $("#message-display p").text("That's a shame.");
-     updateMoney();
-   }
-};
-
-var updateMoney = function() {
-  var money = 0;
-  var moneyHolder = $("#money-update p");
-  var total = moneyHolder.text("$" + money);
-  if (randomCard === cardArray[1]) {
-    money = money + 20;
-    return total;
-  } else if (randomCard !== cardArray[1]) {
-    money = money - 20;
-    return total;
-  }
-};
-
 var pickCard = function() {
   $(".one").click(function() {
-    $(".one").addClass(randomCard);
-    choiceOutcome();
+    $(".one").addClass(shuffleArray(cardArray[0]));
+    if ($(this).hasClass("queen")) {
+    $("#message-display p").text("WOW, I can't believe you found her!");
+    money = money + 20;
+    $("#money-update p").text("$" + money);
+    } else if ($(this).hasClass("joker")) {
+    $("#message-display p").text("That's a shame.");
+    money = money - 20;
+    $("#money-update p").text("$" + money);
+    }
+    setTimeout(function() {
+      $("#message-display p").text("Do you want to Keep Going or Quit?");
+    }, 1500);
+    gameChoice();
+    $(".card").off();
   });
   $(".two").click(function() {
-    $(".two").addClass(randomCard);
-    choiceOutcome();
+    $(".two").addClass(shuffleArray(cardArray[1]));
+    if ($(this).hasClass("queen")) {
+    $("#message-display p").text("WOW, I can't believe you found her!");
+    money = money + 20;
+    $("#money-update p").text("$" + money);
+    } else if ($(this).hasClass("joker")) {
+    $("#message-display p").text("That's a shame.");
+    money = money - 20;
+    $("#money-update p").text("$" + money);
+    }
+    setTimeout(function() {
+      $("#message-display p").text("Do you want to Keep Going or Quit?");
+    }, 1500);
+    gameChoice();
+    $(".card").off();
   });
   $(".three").click(function() {
-    $(".three").addClass(randomCard);
-    choiceOutcome();
+    $(".three").addClass(shuffleArray(cardArray[2]));
+    if ($(this).hasClass("queen")) {
+    $("#message-display p").text("WOW, I can't believe you found her!");
+    money = money + 20;
+    $("#money-update p").text("$" + money);
+    } else if ($(this).hasClass("joker")) {
+    $("#message-display p").text("That's a shame.");
+    money = money - 20;
+    $("#money-update p").text("$" + money);
+    }
+    setTimeout(function() {
+      $("#message-display p").text("Do you want to Keep Going or Quit?");
+    }, 1500);
+    gameChoice();
+    $(".card").off();
   });
+};
+
+var gameChoice = function() {
+  $("#keepgoing-button").click(function() {
+    reRun();
+    $("#keepgoing-button").off();
+  });
+  $("#quit-button").click(function() {
+    $("#message-display p").text("Go on Take your Money and Run");
+    $("#quit-button").off();
+  });
+};
+
+var reRun = function() {
+    $("#message-display p").text("Find the Queen to Win!");
+    cardShow();
 };
 
 $(document).ready(function() {
